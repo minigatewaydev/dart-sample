b8e0e753534fb0d6786b9183d004b2628446d55f
ahmadnazif <ahmadnazif@gmail.com>
2019-09-19 16:33:58 +0800

    cleanup

--- core/RestApiSender.dart
+++ core/RestApiSender.dart
@@ -1,5 +1,3 @@
-import 'dart:convert';
-import 'dart:io';
 import 'dart:async';
 import 'package:http/http.dart' as http;
 import '../models/MtRequest.dart';
@@ -20,22 +18,5 @@ Future<SimpleHttpResponse> postAsync(MtRequest mtReq, String baseUrl) async {
     return new SimpleHttpResponse(resp.statusCode, resp.body, null);
   } catch (e) {
     return new SimpleHttpResponse(99, null, e);
-  }
-}
-
-@deprecated
-Future<String> _readResponseAsync(HttpClientResponse resp) async {
-  try {
-    var completer = new Completer();
-    var contents = new StringBuffer();
-
-    resp.transform(utf8.decoder).listen((data) {
-      contents.write(data);
-    }, onDone: () => completer.complete(contents.toString()));
-
-    var x = await completer.future;
-    return x.toString();
-  } catch (e) {
-    return null;
   }
 }
